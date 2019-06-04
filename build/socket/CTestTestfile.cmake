6773bd7c3dd5efa82f1c6ef3cd215ed8b9540906
haosen9527 <ljhao1994@163..com>
2019-06-04 18:36:55 +0800

    update

--- build/socket/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/micros/snix/src/socket
-# Build directory: /home/micros/snix/build/socket
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
